16792bff1fe2d55e15d91ddb1c59056d309022e5
Adilson Neto <almeidneto@gmail.com>
2019-06-28 08:11:58 -0300

    Add entrypoint main function
    For now the entry point is just parsing the CLI arguments

--- lib/elevio.ex
+++ lib/elevio.ex
@@ -12,7 +12,10 @@ defmodule Elevio do
       :world
 
   """
-  def hello do
-    :world
+  def main(args \\ []) do
+    case OptionParser.parse(args, strict: [keyword: :string]) do
+      {[keyword: keyword_string], _, _} -> IO.puts "Has #{keyword_string}"
+      _ -> IO.puts "where is args???"
+    end
   end
 end
