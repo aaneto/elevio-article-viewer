81d695d3a34c88bda98168e95f85c5404c85ba27
Adilson Neto <almeidneto@gmail.com>
2019-07-03 18:59:56 -0300

    Add argument parsing tests

--- test/elevio_test.exs
+++ test/elevio_test.exs
@@ -2,7 +2,29 @@ defmodule ElevioTest do
   use ExUnit.Case
   doctest Elevio
 
-  test "greets the world" do
-    assert Elevio.hello() == :world
+  test "parse keyword cli argument" do
+    options = Elevio.parseArguments(["--keyword", "key"])
+
+    assert options == [keyword: "key"]
+  end
+
+  test "parse id cli argument" do
+    options = Elevio.parseArguments(["--id", "2"])
+
+    assert options == [id: "2"]
+  end
+
+  test "parse id and keyword cli arguments" do
+    options = Elevio.parseArguments(["--id", "2", "--keyword", "key"])
+
+    assert options == [id: "2", keyword: "key"]
+  end
+
+  test "ignore invalid cli argument" do
+    mispelled_keyword = Elevio.parseArguments(["--keywrd", "foo"])
+    incomplete_id = Elevio.parseArguments(["--id"])
+
+    assert mispelled_keyword == []
+    assert incomplete_id == []
   end
 end
