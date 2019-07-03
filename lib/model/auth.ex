43e3efc7bf7ca03f787e5dd15e7ca3defbc8ebb6
Adilson Neto <almeidneto@gmail.com>
2019-07-03 20:12:19 -0300

    mix format

--- lib/model/auth.ex
+++ lib/model/auth.ex
@@ -1,10 +1,10 @@
 defmodule Elevio.Auth do
-    @enforce_keys [
-      :token,
-      :api_key
-    ]
-    defstruct [
-      :token,
-      :api_key
-    ]
-  end
+  @enforce_keys [
+    :token,
+    :api_key
+  ]
+  defstruct [
+    :token,
+    :api_key
+  ]
+end
