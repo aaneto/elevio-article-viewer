2f27acf5a765011f89f0bae0d46cc4fb07b7c622
Adilson Neto <almeidneto@gmail.com>
2019-07-03 19:47:22 -0300

    Add AsTable implementation for KeywordSearch

--- lib/interfaces/as_table.ex
+++ lib/interfaces/as_table.ex
@@ -2,3 +2,21 @@ defprotocol AsTable do
   @doc "Return representation of object as ASCII Table."
   def as_table(data)
 end
+
+defimpl AsTable, for: Elevio.KeywordSearch do
+  def as_table(search) do
+    tables =
+      Enum.map(search.results, fn result ->
+        Enum.join(
+          [
+            "\ntitle: #{result.title}",
+            "category_id: #{result.category_id}",
+            "id: #{result.id}"
+          ],
+          "\n"
+        )
+      end)
+
+    Enum.join(tables, "\n-----------\n")
+  end
+end
