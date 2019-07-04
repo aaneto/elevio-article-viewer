db6e5730ca7f9b64d161903cd6b8eacdaa3f2314
Adilson Neto <almeidneto@gmail.com>
2019-07-03 21:00:20 -0300

    mix format

--- test/table_view_test.exs
+++ test/table_view_test.exs
@@ -1,29 +1,30 @@
 defmodule TableViewTest do
-    use ExUnit.Case
-    import ExUnit.CaptureIO
+  use ExUnit.Case
+  import ExUnit.CaptureIO
 
-    doctest Elevio.TableView
+  doctest Elevio.TableView
 
-    test "table view clears screen correctly" do
-      expected_output = IO.ANSI.clear() <> IO.ANSI.cursor(0, 0)
-      assert capture_io(fn() -> Elevio.TableView.clear_screen end) == expected_output
-    end
-
-    test "table article 404 display" do
-      auth = %Elevio.Auth{api_key: nil, token: nil}
-      expected_output = "Article does not exist (404).\n"
-      assert capture_io(fn() ->
-        Elevio.TableView.display_single_article(auth, 10)
-      end) == expected_output
-    end
-
-    test "table article standard display" do
-      auth = %Elevio.Auth{api_key: nil, token: nil}
-      valid_id = 0
-      {:ok, article} = Elevio.App.get_article_by_id(auth, valid_id)
-
-      assert capture_io(fn() ->
-        Elevio.TableView.display_single_article(auth, valid_id)
-      end) == AsTable.as_table(article) <> "\n"
-    end
+  test "table view clears screen correctly" do
+    expected_output = IO.ANSI.clear() <> IO.ANSI.cursor(0, 0)
+    assert capture_io(fn -> Elevio.TableView.clear_screen() end) == expected_output
   end
+
+  test "table article 404 display" do
+    auth = %Elevio.Auth{api_key: nil, token: nil}
+    expected_output = "Article does not exist (404).\n"
+
+    assert capture_io(fn ->
+             Elevio.TableView.display_single_article(auth, 10)
+           end) == expected_output
+  end
+
+  test "table article standard display" do
+    auth = %Elevio.Auth{api_key: nil, token: nil}
+    valid_id = 0
+    {:ok, article} = Elevio.App.get_article_by_id(auth, valid_id)
+
+    assert capture_io(fn ->
+             Elevio.TableView.display_single_article(auth, valid_id)
+           end) == AsTable.as_table(article) <> "\n"
+  end
+end
