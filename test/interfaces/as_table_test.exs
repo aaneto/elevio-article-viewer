dcf3dd111556f467f9d75c1fbbce7c303eeacf8d
Adilson Neto <almeidneto@gmail.com>
2019-07-04 22:58:34 -0300

    mix format

--- test/interfaces/as_table_test.exs
+++ test/interfaces/as_table_test.exs
@@ -3,13 +3,13 @@ defmodule AsTableTest do
   doctest AsTable
 
   test "test as table display of article" do
-    article = %Elevio.Article {
+    article = %Elevio.Article{
       id: 2,
       title: "Need a hand?",
       status: "published",
       source: "custom",
       created_at: "2019-06-27T21:35:37Z",
-      author: %Elevio.Author {
+      author: %Elevio.Author{
         name: "Safwan Kamarrudin",
         email: "safwan@elev.io"
       }
@@ -24,29 +24,30 @@ defmodule AsTableTest do
     Author.email: safwan@elev.io
     Created @: 2019-06-27T21:35:37Z
     """
+
     # We need to append a \n to compensante for multistring formattting
     assert AsTable.as_table(article) <> "\n" == article_string
   end
 
   test "test as table display of keyword search" do
-    keyword_search = %Elevio.KeywordSearch {
+    keyword_search = %Elevio.KeywordSearch{
       results: [
-        %Elevio.KeywordResult {
+        %Elevio.KeywordResult{
           title: "BZZZZ",
           category_id: 1,
           id: 6
         },
-        %Elevio.KeywordResult {
+        %Elevio.KeywordResult{
           title: "MOARR PAGES",
           category_id: 1,
           id: 5
         },
-        %Elevio.KeywordResult {
+        %Elevio.KeywordResult{
           title: "Need a hand?",
           category_id: 1,
           id: 2
         },
-        %Elevio.KeywordResult {
+        %Elevio.KeywordResult{
           title: "Another testing article",
           category_id: 1,
           id: 4
@@ -75,6 +76,7 @@ defmodule AsTableTest do
     category_id: 1
     id: 4
     """
+
     # We need to append a \n to compensante for multistring formattting
     assert AsTable.as_table(keyword_search) <> "\n" == article_string
   end
