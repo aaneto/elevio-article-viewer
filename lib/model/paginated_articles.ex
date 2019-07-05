d0e061431b46eb729f9f12351074bb753f0bd79a
Adilson Neto <almeidneto@gmail.com>
2019-07-04 23:18:12 -0300

    Properly parse the paginated article request

--- lib/model/paginated_articles.ex
+++ lib/model/paginated_articles.ex
@@ -6,18 +6,56 @@ defmodule Elevio.PaginatedArticles do
   fashion.
   """
   defstruct [
-    :total_pages,
-    :total_entries,
+    :articles,
     :page_size,
     :page_number,
-    :articles
+    :total_pages,
+    :total_entries
   ]
 
   def decode_from_text(text) do
-    Poison.decode(text,
-      as: %Elevio.PaginatedArticles{
-        articles: [%Elevio.Article{}]
-      }
-    )
+    paginated_articles =
+      Poison.decode!(text,
+        as: %Elevio.PaginatedArticles{
+          articles: [
+            %Elevio.Article{
+              translations: [%Elevio.Translation{}]
+            }
+          ]
+        }
+      )
+
+    {:ok, paginated_articles}
   end
+end
+
+defmodule Elevio.PaginatedArticle do
+  @moduledoc """
+  The PaginatedArticle is a paginated version
+  of an article. This struct is similar to a full
+  article but diverges on a few required fields.
+  """
+  defstruct [
+    :id,
+    :title,
+    :order,
+    :notes,
+    :status,
+    :access,
+    :source,
+    :keywords,
+    :updated_at,
+    :external_id,
+    :category_id,
+    :smart_groups,
+    :has_revision,
+    :translations,
+    :access_emails,
+    :access_domains,
+    :article_groups,
+    :editor_version,
+    :revision_status,
+    :revision_status,
+    :subcategory_groups
+  ]
 end
