2473dafab7313b0d4804ace97bd489d817a45254
Adilson Neto <almeidneto@gmail.com>
2019-07-06 19:23:03 -0300

    Add IO as a parameter into TableView functions to ease integration tests

--- lib/table_view.ex
+++ lib/table_view.ex
@@ -6,31 +6,36 @@ defmodule Elevio.TableView do
   The Table is displayed with ANSI escape characters and can represent
   any data that can be displayed as a key-value pair.
   """
-  def search_by_keyword(auth, keyword, language, page) do
-    clear_screen()
-    IO.puts("Searching articles with keyword #{keyword}")
+  def search_by_keyword(auth, keyword, language, page, io \\ IO) do
+    clear_screen(io)
+    io.puts("Searching articles with keyword #{keyword}")
 
     articles_option = Elevio.App.get_articles_by_keyword(auth, keyword, page, language)
-    IO.puts(display_topic("Page", articles_option))
-    prompt_topic("page", fn new_page -> search_by_keyword(auth, keyword, language, new_page) end)
+    io.puts(display_topic("Page", articles_option))
+
+    prompt_topic(
+      "page",
+      fn new_page -> search_by_keyword(auth, keyword, language, new_page) end,
+      io
+    )
   end
 
-  def search_by_id(auth, id) do
-    clear_screen()
-    IO.puts("Fetching article #{id}")
+  def search_by_id(auth, id, io \\ IO) do
+    clear_screen(io)
+    io.puts("Fetching article #{id}")
 
     display_article_option = Elevio.App.get_article_by_id(auth, id)
-    IO.puts(display_topic("id", display_article_option))
-    prompt_topic("id", fn new_id -> search_by_id(auth, new_id) end)
+    io.puts(display_topic("id", display_article_option))
+    prompt_topic("id", fn new_id -> search_by_id(auth, new_id) end, io)
   end
 
-  def show_articles_paginated(auth, page_number) do
-    clear_screen()
-    IO.puts("Paginating all articles")
+  def show_articles_paginated(auth, page_number, io \\ IO) do
+    clear_screen(io)
+    io.puts("Paginating all articles")
 
     show_articles_option = Elevio.App.get_paginated_articles(auth, page_number)
-    IO.puts(display_topic("Page", show_articles_option))
-    prompt_topic("page", fn new_page -> show_articles_paginated(auth, new_page) end)
+    io.puts(display_topic("Page", show_articles_option))
+    prompt_topic("page", fn new_page -> show_articles_paginated(auth, new_page) end, io)
   end
 
   def display_topic(topic, as_table_data_option) do
@@ -52,29 +57,29 @@ defmodule Elevio.TableView do
     end
   end
 
-  def prompt_topic(topic, next_display) do
+  def prompt_topic(topic, next_display, io \\ IO) do
     prompt_text = """
     e: exit,
     g #{topic}: goto new #{topic}
     """
 
-    case prompt_text |> IO.gets() |> String.trim() do
+    case prompt_text |> io.gets() |> String.trim() do
       "g" <> new_topic_id_str ->
         case Integer.parse(String.trim(new_topic_id_str)) do
           :error ->
-            IO.puts("Could not parse page #{new_topic_id_str}")
+            io.puts("Could not parse page #{new_topic_id_str}")
 
           {new_topic_id, _} ->
             next_display.(new_topic_id)
         end
 
       _ ->
-        IO.puts("Exitting. ")
+        io.puts("Exitting. ")
     end
   end
 
-  def clear_screen do
-    IO.write(IO.ANSI.clear())
-    IO.write(IO.ANSI.cursor(0, 0))
+  def clear_screen(io \\ IO) do
+    io.write(IO.ANSI.clear())
+    io.write(IO.ANSI.cursor(0, 0))
   end
 end
