c440e8378d5d39daf42d8863e13042fa90555c0a
Adilson Neto <almeidneto@gmail.com>
2019-07-07 01:59:01 -0300

    Add integration tests for use cases of the app

--- test/elevio_test.exs
+++ test/elevio_test.exs
@@ -2,6 +2,8 @@ defmodule ElevioTest do
   use ExUnit.Case
   doctest Elevio
 
+  alias Elevio.FakeIO
+
   test "parse keyword cli argument" do
     options = Elevio.parseArguments(["--keyword", "key"])
 
@@ -26,5 +28,89 @@ defmodule ElevioTest do
 
     assert mispelled_keyword == []
     assert incomplete_id == []
+  end
+
+  # Integration Tests
+
+  test "user inputed keyword and id" do
+    FakeIO.start_io("e")
+    Elevio.main(["--id", "1", "--keyword", "key"], FakeIO)
+
+    assert FakeIO.get_input() == Elevio.getHelpMenu() <> "\n"
+  end
+
+  test "user inputed invalid id" do
+    FakeIO.start_io("e")
+    Elevio.main(["--id", "foobar"], FakeIO)
+
+    assert FakeIO.get_input() == "Invalid ID. Exitting.\n"
+  end
+
+  test "user searched by keyword without providing language" do
+    FakeIO.start_io("e")
+    Elevio.main(["--keyword", "foobar"], FakeIO)
+
+    assert FakeIO.get_input() == "Please provide a language for the keyword search.\n"
+  end
+
+  test "user provided language without providing keyword" do
+    FakeIO.start_io("e")
+    Elevio.main(["--language", "en"], FakeIO)
+
+    assert FakeIO.get_input() == "Cannot query articles by languague, please provide a keyword.\n"
+  end
+
+  test "user searched by id" do
+    auth = %Elevio.Auth{
+      api_key: nil,
+      token: nil
+    }
+
+    FakeIO.start_io("e")
+    Elevio.main(["--id", "1"], FakeIO)
+
+    main_output = FakeIO.get_input()
+    FakeIO.reset_captured_input()
+
+    Elevio.TableView.search_by_id(auth, 1, FakeIO)
+    id_search_output = FakeIO.get_input()
+
+    assert id_search_output == main_output
+  end
+
+  test "user searched by keyword" do
+    auth = %Elevio.Auth{
+      api_key: nil,
+      token: nil
+    }
+
+    FakeIO.start_io("e")
+    Elevio.main(["--keyword", "other", "--language", "en"], FakeIO)
+
+    main_output = FakeIO.get_input()
+    FakeIO.reset_captured_input()
+
+    Elevio.TableView.search_by_keyword(auth, "other", "en", 1, FakeIO)
+    keyword_search_output = FakeIO.get_input()
+
+    assert keyword_search_output == main_output
+  end
+
+  test "user requested all articles" do
+    auth = %Elevio.Auth{
+      api_key: nil,
+      token: nil
+    }
+
+    FakeIO.start_io("e")
+    Elevio.main(["--all"], FakeIO)
+
+    main_output = FakeIO.get_input()
+    FakeIO.reset_captured_input()
+
+    Elevio.TableView.show_articles_paginated(auth, 1, FakeIO)
+    paginated_output = FakeIO.get_input()
+
+    assert paginated_output == main_output
   end
 end
